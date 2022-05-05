cb4eb8d9a852e4f444a125f044a0654fdbe1b86f
777Archer <101377287+777Archer@users.noreply.github.com>
2022-05-05 05:37:57 -0700

    Add files via upload

---
+++ Celsius-to-Fahrenheit-Converter.py
@@ -0,0 +1,4 @@
+
+temperature_in_celsius = (float (input("Please enter a temperature in Celsius:\n")))
+temperature_in_fahrenheit = (temperature_in_celsius * 1.8) + 32
+print("%.1f Celsius = %.1f Fahrenheit" %(temperature_in_celsius,temperature_in_fahrenheit))
